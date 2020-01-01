9527dca9aea9531c3bbae02797d590f01d5ce89d
Justin <jiajian92@gmail.com>
2020-01-01 15:35:12 +0800

    Generate new tf record and update config

--- utilities/split_label.py
+++ utilities/split_label.py
@@ -10,9 +10,15 @@ grouped.apply(lambda x: len(x)).value_counts()
 gb = full_labels.groupby('filename')
 grouped_list = [gb.get_group(x) for x in gb.groups]
 
-train_index = np.random.choice(len(grouped_list), size=6, replace=False)
-test_index = np.setdiff1d(list(range(10)), train_index)
-# take first 200 files
+total_size = len(grouped_list)
+train_size = int(total_size * 0.7)
+test_size = int(total_size * 0.3)
+print(train_size, test_size)
+
+train_index = np.random.choice(len(grouped_list), size=train_size, replace=False)
+test_index = np.setdiff1d(list(range(total_size)), train_index)
+print('Train index: {}\nTest index: {}'.format(train_index, test_index))
+
 train = pd.concat([grouped_list[i] for i in train_index])
 test = pd.concat([grouped_list[i] for i in test_index])
 
